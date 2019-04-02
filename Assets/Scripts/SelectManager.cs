7310554185e2b313b5db62103ccd68f881b8be50
Hugo <hbuissez@gmail.com>
2019-04-02 21:59:04 +0200

    Add crosshair + raycast for 'werewolf' tag object
    J'ai add un reticule pour viser et j'ai ajouté un raycast qui fait que les object qu'on tagera 'werewolf' pourrons activer des truc quand on les pointera (quand on passera la souris sur eux)

---
+++ Assets/Scripts/SelectManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectManager : MonoBehaviour
+{
+    // Here objects w/ tag 'Werewolf' change their Material when we point them.
+    [SerializeField] private string selectableTag = "Werewolf";
+    [SerializeField] private Material other;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    void Update()
+    {
+        
+        if (_selection != null)
+        {
+            var selectRenderer = _selection.GetComponent<Renderer>();
+            selectRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+            
+            
+        var ray = Camera.current.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var select = hit.transform;
+            if (select.CompareTag(selectableTag))
+            {
+                var selectRenderer = select.GetComponent<Renderer>();
+                if (selectRenderer != null)
+                {
+                    selectRenderer.material = other;
+                }
+
+                _selection = select;
+            }
+            
+            
+        }
+
+    }
+}

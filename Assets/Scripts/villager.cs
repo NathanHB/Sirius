6a8d7191cbdd0ead57c1fb08d56eb51b6c61c46a
Hugo <hbuissez@gmail.com>
2019-04-15 18:45:51 +0200

    add prefab

---
+++ Assets/Scripts/villager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class villager : MonoBehaviour
+{
+    public bool isAlive = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

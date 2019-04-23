ac73b04b1cff972c5c436602b0ad98f34e84cc0e
jope75 <jopellegrini2@gmail.com>
2019-04-24 00:29:56 +0200

    map changes

--- Assets/Scripts/dayNightCycle1.cs
+++ Assets/Scripts/dayNightCycle1.cs
@@ -7,18 +7,19 @@ public class dayNightCycle1 : MonoBehaviour
     public Light sun; 
     public ParticleSystem stars;
     public GameObject starsObj;
-
+    private float timer=0;
        
 
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         OrbitSun(); 
     }
     
     void OrbitSun(){
         
-        transform.RotateAround(new Vector3(250,250,0), Vector3.forward,  2f*Time.deltaTime);        
+        transform.RotateAround(new Vector3(250,250,0), Vector3.forward,  Time.deltaTime);        
     }
 }
